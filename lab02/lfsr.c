a369932d67aa63b58ec8641a951548eb6ad8dd5b
xiahua19 <jx.zhang@pku.edu.cn>
2024-01-13 12:49:57 +0800

    finish lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,21 @@
 #include <string.h>
 #include "lfsr.h"
 
+uint16_t get_bit(uint16_t x,
+                 uint16_t n) {
+    return (x & (1 << n)) >> n;
+}
+
+void set_bit(uint16_t * x,
+             uint16_t n,
+             uint16_t v) {
+    uint16_t mask = ~(1 << n);
+    (*x) = ((*x) & mask) | (v << n);
+}
+
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    unsigned instert_bit = get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+    (*reg) = (*reg) >> 1;
+    set_bit(reg, 15, instert_bit);
 }
 
